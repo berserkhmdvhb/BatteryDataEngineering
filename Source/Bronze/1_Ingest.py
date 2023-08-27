1328a53e2a0b211b7f01cc134ab96b020f2c5dd8
berserkhmdvhb <berserkhmdvhb>
2023-08-27 08:48:16 +0000

    Store imported data as delta tables

--- Source/Bronze/1_Ingest.py
+++ Source/Bronze/1_Ingest.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Setup and Imports
+
+# COMMAND ----------
+
 import requests
 from pyspark import SparkFiles
 
@@ -11,11 +16,32 @@ import sys
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC # Import from Github
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 1. NASA
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Import Metadata
+
+# COMMAND ----------
+
 url = "https://raw.githubusercontent.com/berserkhmdvhb/BatteryDatasets/main/Datasets/NASA/metadata.csv"
 sc.addFile(url)
 path  = SparkFiles.get("metadata.csv")
-metadata_df = spark.read.csv("file://" + path, header=True, inferSchema= True)
+metadata_df = spark.read.csv("file://" + path, header = True, inferSchema = True)
 profiles = [row.type for row in metadata_df.select('type').collect()]
+ambient_temperatures = [row.ambient_temperature for row in metadata_df.select('ambient_temperature').collect()] 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Import Batteries Files
 
 # COMMAND ----------
 
@@ -26,41 +52,81 @@ if response.status_code == 200:
     directories = response.json()['payload']['tree']['items']
     csv_dirs = [repo + directories[i]['path'] for i in range(len(directories))]
     file_names = [directories[i]['name'] for i in range(len(directories))]
+
     # initialize discharge dataframe
     url = csv_dirs[0]
     sc.addFile(url)
-    path  = SparkFiles.get(file_names[0])
-    batteries_discharge = spark.read.csv("file://" + path, header=True, inferSchema= True).withColumn("Profile", lit(profiles[0])).withColumn("Idx", lit(0)).withColumn("file", lit(file_names[0]))
+    path = SparkFiles.get(file_names[0])
+    batteries_discharge = (spark
+                           .read
+                           .csv("file://" + path, header = True, inferSchema = True)
+                           .withColumn("Profile", lit(profiles[0]))
+                           .withColumn("Idx", lit(0))
+                           .withColumn("file", lit(file_names[0]))
+                           .withColumn("ambient_temperature", lit(ambient_temperatures[0]))
+                           
+    )
     # initialize impedance dataframe
     url = csv_dirs[1]
     sc.addFile(url)
-    path  = SparkFiles.get(file_names[1])
-    batteries_impedance = spark.read.csv("file://" + path, header=True, inferSchema= True).withColumn("Profile", lit(profiles[1])).withColumn("Idx", lit(1)).withColumn("file", lit(file_names[1]))
+    path = SparkFiles.get(file_names[1])
+    batteries_impedance = (spark
+                           .read
+                           .csv("file://" + path, header = True, inferSchema = True)
+                           .withColumn("Profile", lit(profiles[1]))
+                           .withColumn("Idx", lit(0))
+                           .withColumn("file", lit(file_names[1]))
+                           .withColumn("ambient_temperature", lit(ambient_temperatures[1]))
+                           
+    )
     # initialize charge dataframe
     url = csv_dirs[2]
     sc.addFile(url)
-    path  = SparkFiles.get(file_names[2])
-    batteries_charge = spark.read.csv("file://" + path, header=True, inferSchema= True).withColumn("Profile", lit(profiles[2])).withColumn("Idx", lit(2)).withColumn("file", lit(file_names[2]))    
+    path = SparkFiles.get(file_names[2])
+    batteries_charge = (spark
+                           .read
+                           .csv("file://" + path, header = True, inferSchema = True)
+                           .withColumn("Profile", lit(profiles[2]))
+                           .withColumn("Idx", lit(0))
+                           .withColumn("file", lit(file_names[2]))
+                           .withColumn("ambient_temperature", lit(ambient_temperatures[2]))
+                           
+    )  
     for i in range(3, len(directories)):
         url = csv_dirs[i]
         sc.addFile(url)
         path  = SparkFiles.get(file_names[i])
-        df_temp = spark.read.csv("file://" + path, header=True, inferSchema= True).withColumn("Profile", lit(profiles[i])).withColumn("Idx", lit(i)).withColumn("file", lit(file_names[i]))
+        df_temp = (spark
+                   .read
+                   .csv("file://" + path, header = True, inferSchema = True)
+                   .withColumn("Profile", lit(profiles[i]))
+                   .withColumn("Idx", lit(i))
+                   .withColumn("file", lit(file_names[i]))
+                   .withColumn("ambient_temperature", lit(ambient_temperatures[i]))
+        )
         if profiles[i] == "discharge":
             batteries_discharge = batteries_discharge.union(df_temp)
         elif profiles[i] == "charge":
             batteries_charge = batteries_charge.union(df_temp)
-        elif profiles[i] == "impdeance":
+        elif profiles[i] == "impedance":
             batteries_impedance = batteries_impedance.union(df_temp)
 
 # COMMAND ----------
 
-batteries_discharge.count()
+# MAGIC %md
+# MAGIC ### Store as Delta Tables
 
 # COMMAND ----------
 
-len(directories)
+# MAGIC %sql
+# MAGIC --CREATE SCHEMA LION
 
 # COMMAND ----------
 
+schema = "LION"
 
+# COMMAND ----------
+
+batteries_impedance.write.format('delta').mode('overwrite').saveAsTable(f'{schema}.NASA_batteries_impedance')
+batteries_charge.write.format('delta').mode('overwrite').saveAsTable(f'{schema}.NASA_batteries_charge')
+batteries_discharge.write.format('delta').mode('overwrite').saveAsTable(f'{schema}.NASA_batteries_discharge')
